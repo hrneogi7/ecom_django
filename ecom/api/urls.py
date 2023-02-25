1526494ec5e636a491c950f31a4143f0b301d87e
Hrithik Neogi <56794641+Hrithik0699@users.noreply.github.com>
2023-02-25 19:32:01 +0530

    payment gateway implemented

--- ecom/api/urls.py
+++ ecom/api/urls.py
@@ -8,5 +8,6 @@ urlpatterns=[
     path('products/', include('api.product.urls')),
     path('user/', include('api.user.urls')),
     path('order/', include('api.order.urls')),
+    path('payment/', include('api.payment.urls')),
     path('api-token-auth/', views.obtain_auth_token, name='api-token-auth')
 ]
